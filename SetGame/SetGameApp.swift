58b064e45dbf1540f79ab18f867de20f3cbcfd81
samuelalake <harlahke@gmail.com>
2022-01-21 04:45:20 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Samuel Alake on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
